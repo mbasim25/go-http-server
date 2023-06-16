63682da60085a466e6eac5cb1d825a12cbf04b0e
MohB25 <mohammedbasim2525@gmail.com>
2023-06-16 22:41:05 +0300

    renamed package

--- posts.go
+++ posts.go
@@ -1,28 +1,33 @@
-package main
+package posts
 
 type Post struct {
-	ID      int    `db:"id"`
-	content string `db:"content"`
+	ID      int64  `db:"id"`
+	Content string `db:"content"`
 }
 
 type Comment struct {
-	ID      int    `db:"id"`
-	post_id int    `db:"post_id"`
-	body    string `db:"body"`
+	ID     int64  `db:"id"`
+	PostId int64  `db:"post_id"`
+	Body   string `db:"body"`
 }
 
 type PostStore interface {
-	Post(id int) (Post, error)
+	Post(id int64) (Post, error)
 	Posts() ([]Post, error)
 	CreatePost(p *Post) error
 	UpdatePost(p *Post) error
-	DeletePost(id int) error
+	DeletePost(id int64) error
 }
 
 type CommentStore interface {
-	Comment(id int) (Comment, error)
-	CommentsByPost(post_id int) ([]Comment, error)
+	Comment(id int64) (Comment, error)
+	CommentsByPost(postId int64) ([]Comment, error)
 	CreateComment(c *Comment) error
 	UpdateComment(c *Comment) error
-	DeleteComment(id int) error
+	DeleteComment(id int64) error
+}
+
+type Store interface {
+	PostStore
+	CommentStore
 }
